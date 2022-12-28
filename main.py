9840b77a239956e779011156464e343732213049
AnrPg <frnephon@mail.com>
2022-12-28 21:19:14 +0200

    Created mock paths for fetching and creating users, using a newly-created services module

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 import fastapi
 import uvicorn
+import shortuuid
 from typing import List
-from schemas.user import UserView
+
+from schemas.user import UserView, UserDetailed
+from services import users
+
+from datetime import datetime, timedelta
 
 router = fastapi.FastAPI()
 
@@ -11,7 +16,7 @@ def read_items() -> List[UserView]:
     """
     Retrieve user.
     """
-
+    response = [users.get_user()]
     # response = [UserView(id='1', firstName='Anr', lastName='Pg', email='anr@email.com'), UserView(id='2', firstName='Maria', lastName='Annou', email='ma@email.com')]
     return response
 
@@ -20,9 +25,24 @@ def create_user() -> str:
     """
     Create a new user object and store it at the db.
     User related form headings:
-    !!!!!!!!!!!!!!    
+    id: str
+    firstName: str
+    lastName: str
+    password: str
+    email: Optional[str]
+    tel: Optional[str]
+    birthdate: datetime
+    nationality: List[str]
+    sex: str    
     """
-    
+
+    userToBeCreated = UserDetailed(id=shortuuid.uuid().__str__(), firstName="troll", lastName="tril", email="", tel="", password="", birthdate=datetime.now()-timedelta(days=2), nationality=[""], sex="", dateCreated=datetime.now(), dateLastLogin=datetime.now()+timedelta(days=1))
+    try:
+        response = users.add_user(userToBeCreated)
+    except:
+        pass
+
+    return response
 
 if __name__ == '__main__':
     uvicorn.run(router, port=8000, host='127.0.0.1')
--- services/users.py
+++ services/users.py
@@ -1,2 +1,12 @@
-from ..schemas import user
 
+
+from schemas.user import UserDetailed
+
+__cache = []
+
+def get_user() -> UserDetailed:
+    return  __cache[0] #f"{{\"response\":\"Success!\", \"user id\":\"{user_to_create.id}\"}}" 
+
+def add_user(user_to_create: UserDetailed) -> str:
+    __cache.append(user_to_create)
+    return f"{{\"response\":\"Success!\", \"user id\":\"{user_to_create.id}\"}}"
